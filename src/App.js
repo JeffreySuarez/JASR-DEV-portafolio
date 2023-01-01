1a30065c7f437822e4e80f3c465aa7bb47acb8b2
jeffrey suarez <jasr7221@gmail.com>
2023-01-01 16:35:18 -0500

    ajuste del home

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/home" element={<Layout />}>
-          <Route index exact path="/home" element={<Home />} />
+        <Route exact path="/" element={<Layout />}>
+          <Route index exact path="/" element={<Home />} />
           <Route exact path="/services" element={<Services />} />
           <Route exact path="/portfolio" element={<Portfolio />} />
           <Route exact path="/resume" element={<Resume />} />
