9281b4409cdee35f7b97afa39d2d1aef07392fd9
jeffrey suarez <79020208+JeffreySuarez@users.noreply.github.com>
2023-06-24 14:20:15 -0500

    actualizacion CV

--- src/components/Header/components/CV.js
+++ src/components/Header/components/CV.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Document from "../../../assets/doc/CVJeffreySuarez.pdf";
+import Document from "../../../assets/doc/CVJeffreySuarezRey.pdf";
 
 const CV = () => {
   return (
